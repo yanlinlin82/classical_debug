d85af14c97355d14e67d82f873abe914b79fcfda
Linlin Yan <yanlinlin82@gmail.com>
2018-03-18 02:05:10 +0800

    Show flags

--- src/Registers.cpp
+++ src/Registers.cpp
@@ -6,8 +6,17 @@ void Registers::Dump()
 {
 	printf("AX=%04X  BX=%04X  CX=%04X  DX=%04X  SP=%04X  BP=%04X  SI=%04X  DI=%04X\n",
 			regs_[AX], regs_[BX], regs_[CX], regs_[DX], regs_[SP], regs_[BP], regs_[SI], regs_[DI]);
-	printf("DS=%04X  ES=%04X  SS=%04X  CS=%04X  IP=%04X   NV UP DI PL NZ NA PO NC\n",
-			regs_[DS], regs_[ES], regs_[SS], regs_[CS], regs_[IP]);
+	printf("DS=%04X  ES=%04X  SS=%04X  CS=%04X  IP=%04X   %s %s %s %s %s %s %s %s\n",
+			regs_[DS], regs_[ES], regs_[SS], regs_[CS], regs_[IP],
+			((regs_[FLAGS] & 0x0800) != 0 ? "OV" : "NV"), // Bit 11 (of): 1000 0000 0010 = 802 - Overflow (OV)
+			((regs_[FLAGS] & 0x0400) != 0 ? "DN" : "UP"), // Bit 10 (df): 0100 0000 0010 = 402 - Direction Down (DN)
+			((regs_[FLAGS] & 0x0200) != 0 ? "EI" : "DI"), // Bit  9 (if): 0010 0000 0010 = 202 - Interrupt Enabled (EI)
+			((regs_[FLAGS] & 0x0080) != 0 ? "NG" : "PL"), // Bit  7 (sf): 0000 1000 0010 =  82 - Sign Negative (NG)
+			((regs_[FLAGS] & 0x0040) != 0 ? "ZR" : "NZ"), // Bit  6 (zf): 0000 0100 0010 =  42 - Zero (ZR)
+			((regs_[FLAGS] & 0x0010) != 0 ? "AC" : "NA"), // Bit  4 (af): 0000 0001 0010 =  12 - Auxiliary Carry (AC)
+			((regs_[FLAGS] & 0x0004) != 0 ? "PE" : "PO"), // Bit  2 (pf): 0000 0000 0110 =  06 - Parity Even (PE)
+			((regs_[FLAGS] & 0x0001) != 0 ? "CY" : "NC")  // Bit  0 (cf): 0000 0000 0011 =  03 - Carry (CY)
+			);
 	printf("%04X:%04X F60000        TEST    BYTE PTR [BX+SI],00                  DS:0000=CD\n", regs_[CS], regs_[IP]);
 }
 
